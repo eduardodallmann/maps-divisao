7a4069e3bce84a2943ebe8241a984fc259fc0db4
Eduardo Dallmann <eduardodallmann@gmail.com>
2024-10-17 15:58:17 -0300

    remove auto coordenadas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,12 @@
 
 import { MyMap } from '~/components/map';
 import { getDivisaoAtualData, getMenData, getStreetsData } from '~/infra/sheet';
-import { completeCoordenatesStreets } from '~/services/complete-coordenates';
 
 export default async function Home() {
   const apiKey = process.env.API_KEY || '';
   const data = await getStreetsData();
   const menData = await getMenData();
   const divisaoAtual = await getDivisaoAtualData();
-
-  await completeCoordenatesStreets({ data });
 
   return (
     <MyMap
