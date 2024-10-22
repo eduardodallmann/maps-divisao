c4e767af9f858adfc278afb1202f44baaa420c94
Eduardo Dallmann <eduardodallmann@gmail.com>
2024-10-21 23:02:13 -0300

    add dynamic route

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,8 @@ export const metadata: Metadata = {
   title: 'Mapa da nova formação de congregação',
 };
 
+export const dynamic = 'force-dynamic';
+
 export default function RootLayout({
   children,
 }: Readonly<{
