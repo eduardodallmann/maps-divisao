eb1ead7b290ac8f0b1659287cd2669e838605503
Eduardo Dallmann <eduardodallmann@gmail.com>
2024-12-02 09:29:55 -0300

    add @vercel/analytics for improved site analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next';
 
+import { Analytics } from '@vercel/analytics/react';
+
 import '../styles/global.css';
 
 export const metadata: Metadata = {
@@ -21,7 +23,10 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=block"
         />
       </head>
-      <body className={`antialiased`}>{children}</body>
+      <body className={`antialiased`}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
